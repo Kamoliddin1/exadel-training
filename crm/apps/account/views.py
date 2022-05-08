89fe1e457387edb9d09673b902077c69da42d97b
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-09 02:43:40 +0500

    FBV and CBV some examples

--- crm/apps/account/views.py
+++ crm/apps/account/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, View
 
-# Create your views here.
+from .models import Company, Branch, User, City
+
+
+def index(request):
+    context = {'index': 'Hello there'}
+    return render(request, 'base.html', context)
+
+
+class BranchListView(ListView):
+    model = Branch
+    queryset = Branch.objects.all()
+
+
+class UserListView(ListView):
+    model = User
+    queryset = User.objects.filter(user__is_user=True)
+
+
+class CompanyListView(ListView):
+    model = Company
+    queryset = Company.objects.filter(company__is_company=True)
