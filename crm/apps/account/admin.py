5b8f390100b48e0e557136be93043287916edd6f
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-09 02:42:27 +0500

    register models

--- crm/apps/account/admin.py
+++ crm/apps/account/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    City, Country, User,
+    Company, Branch, Rating, Account
+)
 
-# Register your models here.
+admin.site.register(City)
+admin.site.register(Country)
+admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(Branch)
+admin.site.register(Rating)
+admin.site.register(Account)
