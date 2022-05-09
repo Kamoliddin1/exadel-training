551411e23e755439084decf1948c3a74c7eb7cb6
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-09 11:44:45 +0500

    Create company

--- crm/apps/account/urls.py
+++ crm/apps/account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index, BranchListView, CompanyListView, UserListView
+from .views import (index, BranchListView, CompanyListView,
+                    UserListView, CompanyCreateView)
 
 urlpatterns = [
     path('', index, name='index'),
     path('branches', BranchListView.as_view(), name='branches'),
     path('companies', CompanyListView.as_view(), name='companies'),
     path('users', UserListView.as_view(), name='users'),
+    path('create', CompanyCreateView.as_view(), name="create"),
 ]
--- crm/apps/account/views.py
+++ crm/apps/account/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, View
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, View, CreateView
 
-from .models import Company, Branch, User, City
+from .models import Company, Branch, User, City, Account
 
 
 def index(request):
@@ -22,3 +22,26 @@ class UserListView(ListView):
 class CompanyListView(ListView):
     model = Company
     queryset = Company.objects.filter(company__is_company=True)
+
+
+class CompanyCreateView(CreateView):
+    model = Company
+    fields = ['title', 'company', 'city', 'street']
+
+    def post(self, request, *args, **kwargs):
+        title = request.POST['title']
+        company = request.POST['company']
+        city = request.POST['city']
+        street = request.POST['street']
+        try:
+            city_ins = get_object_or_404(City, id=city)
+        except City.DoesNotExist:
+            city_ins = 1
+        try:
+            company_ins = get_object_or_404(Account, id=company)
+        except City.DoesNotExist:
+            company_ins = 1
+        obj = Company.objects.create(title=title, company=company_ins, city=city_ins, street=street)
+
+        obj.save()
+        return redirect('companies')
