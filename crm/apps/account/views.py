33c78677a241f7e80f43c9bfa389d3939b534843
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-12 12:01:00 +0500

    Adding 3 types of crud views
    function, class, generic based views

--- crm/apps/account/views.py
+++ crm/apps/account/views.py
@@ -1,47 +1,97 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, View, CreateView
 
-from .models import Company, Branch, User, City, Account
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.generics import (ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView,)
 
-
-def index(request):
-    context = {'index': 'Hello there'}
-    return render(request, 'base.html', context)
-
-
-class BranchListView(ListView):
-    model = Branch
-    queryset = Branch.objects.all()
+from .models import Company, Branch, User, City, Account, Country
+from .serializers import CountrySerializer, CountryCreateSerializer, CountryUpdateSerializer, CountryCRUDSerializer
 
 
-class UserListView(ListView):
-    model = User
-    queryset = User.objects.filter(user__is_user=True)
+@api_view(['GET', 'POST'])
+def country_list(request):
+    if request.method == 'GET':
+        countries = Country.objects.all()
+        serializer = CountrySerializer(countries, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        serializer = CountryCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class CompanyListView(ListView):
-    model = Company
-    queryset = Company.objects.filter(company__is_company=True)
+@api_view(['GET', 'PUT', 'DELETE'])
+def country_detail(request, pk):
+    try:
+        country = Country.objects.get(pk=pk)
+    except Country.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CountrySerializer(country)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'PUT':
+        serializer = CountryUpdateSerializer(country, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        country.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CompanyCreateView(CreateView):
-    model = Company
-    fields = ['title', 'company', 'city', 'street']
+class CountryListView(APIView):
+    def get(self, request):
+        countries = Country.objects.all()
+        serializer = CountrySerializer(countries, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args, **kwargs):
-        title = request.POST['title']
-        company = request.POST['company']
-        city = request.POST['city']
-        street = request.POST['street']
+    def post(self, request):
+        serializer = CountryCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CountryDetailView(APIView):
+    def get_object(self, pk):
         try:
-            city_ins = get_object_or_404(City, id=city)
-        except City.DoesNotExist:
-            city_ins = 1
-        try:
-            company_ins = get_object_or_404(Account, id=company)
-        except City.DoesNotExist:
-            company_ins = 1
-        obj = Company.objects.create(title=title, company=company_ins, city=city_ins, street=street)
+            country = Country.objects.get(pk=pk)
+            return country
+        except Country.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        obj.save()
-        return redirect('companies')
+    def get(self, request, pk):
+        obj = self.get_object(pk)
+        serializer = CountrySerializer(obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        obj = self.get_object(pk)
+        serializer = CountryUpdateSerializer(obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        obj = self.get_object(pk)
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CountryListCreateAPIView(ListCreateAPIView):
+    serializer_class = CountryCRUDSerializer
+    queryset = Country.objects.all()
+
+
+class CountryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = CountryCRUDSerializer
+    queryset = Country.objects.all()
