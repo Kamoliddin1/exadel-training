ddcea27006f49cc1c4c71a084c4604baf56a9317
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-12 12:01:33 +0500

    registered views
    tested all of them worked

--- crm/apps/account/urls.py
+++ crm/apps/account/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import (index, BranchListView, CompanyListView,
-                    UserListView, CompanyCreateView)
+
+# from .views import (index, BranchListView, CompanyListView,
+#                     UserListView, CompanyCreateView)
+from .views import country_list, country_detail, CountryListView, CountryDetailView, CountryListCreateAPIView, \
+    CountryRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('branches', BranchListView.as_view(), name='branches'),
-    path('companies', CompanyListView.as_view(), name='companies'),
-    path('users', UserListView.as_view(), name='users'),
-    path('create', CompanyCreateView.as_view(), name="create"),
+    path('country', country_list, name='country_list'),
+    path('country/<int:pk>', country_detail, name='country_detail'),
+    path('country2/', CountryListView.as_view(), name='countries_listview'),
+    path('country2/<int:pk>', CountryDetailView.as_view(), name='countries_detailview'),
+    path('country3/', CountryListCreateAPIView.as_view(), name='countries_list_apiview'),
+    path('country3/<int:pk>', CountryRetrieveUpdateDestroyAPIView.as_view(), name='countries_list_apiview'),
 ]
