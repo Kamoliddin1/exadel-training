7b179fb3f8e51720422ec602e84aeab7af2850c8
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-17 03:13:52 +0500

    add jwt to settings

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = "account.Account"
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
