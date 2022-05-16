1450b2d8785d8cca4ffae2485f66feaf46ee29d6
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-16 15:55:34 +0500

    alter to modelserializer

--- crm/apps/account/serializers/account.py
+++ crm/apps/account/serializers/account.py
@@ -2,23 +2,8 @@ from rest_framework import serializers
 from ..models import Account
 
 
-class AccountSerializer(serializers.Serializer):
-    id = serializers.IntegerField(required=False)
-    username = serializers.CharField(max_length=150)
-    is_user = serializers.BooleanField(default=False)
-    is_company = serializers.BooleanField(default=False)
+class AccountSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        account = Account(**validated_data)
-        account.save()
-        return account
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.username = validated_data.get('username', instance.username)
-        instance.is_user = validated_data.get('is_user', instance.is_user)
-        instance.is_company = validated_data.get('is_company', instance.is_company)
-
-        instance.save()
-        return instance
+    class Meta:
+        model = Account
+        fields = ['id', 'username', 'is_user', 'is_company']
