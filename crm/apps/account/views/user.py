fdc2e55cf68e818d4ab60153832ded810042f279
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-16 15:58:47 +0500

    user modelserializer

--- crm/apps/account/views/user.py
+++ crm/apps/account/views/user.py
@@ -1,15 +1,9 @@
-from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView, )
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from ..models import User
 from ..serializers.user import UserSerializer
 
 
-class UserListCreateAPIView(ListCreateAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-
-class UserDetailAPIView(RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
