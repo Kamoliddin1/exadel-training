b078ab817f7e6210c5d325ec48024191da8290a6
kamoliddin chinaliyev <Chkb007147@gmail.com>
2022-05-29 03:08:05 +0500

    move to class based tests

--- crm/apps/account/tests/account/test_account.py
+++ crm/apps/account/tests/account/test_account.py
@@ -2,18 +2,66 @@ import pytest
 from rest_framework import status
 import json
 
-from rest_framework.reverse import reverse
-
 pytest_plugins = [
-    "apps.account.tests.fixtures.user",
-
+    "apps.account.tests.fixtures.account",
 ]
+pytestmark = pytest.mark.django_db
 
 
-@pytest.mark.django_db
-def test_accounts_list(auto_login) -> None:
-    """  Test for listing all account both companies and clients  """
+class TestAccountEndpoints:
     url = '/accounts/'
-    client, user = auto_login()
-    response = client.get(url)
-    assert response.status_code == status.HTTP_200_OK
+
+    def test_accounts_list(self, auto_login) -> None:
+        """  Test for listing all account both companies and clients  """
+        client, user = auto_login()
+        response = client.get(self.url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_accounts_retrieve(self, auto_login, create_client) -> None:
+        expected_json = {
+            'id': create_client.id,
+            'username': create_client.username,
+            'is_user': create_client.is_user,
+            'is_company': create_client.is_company,
+            'password': create_client.password
+        }
+        client, user = auto_login()
+        response = client.get(f"{self.url}{create_client.id}/")
+        data = json.loads(response.content)
+        assert response.status_code == status.HTTP_200_OK
+        assert data == expected_json
+
+    def test_accounts_update(self, auto_login, create_client) -> None:
+        expected_json = {
+            "username": create_client.username + '1',
+            "password": create_client.password,
+            "is_user": create_client.is_user,
+            "is_company": create_client.is_company
+        }
+        client, user = auto_login()
+        response = client.put(f"{self.url}{create_client.id}/", data=expected_json, format='json')
+        data = json.loads(response.content)
+        assert response.status_code == status.HTTP_200_OK
+        assert data['username'] == expected_json['username']
+        assert data['is_user'] == expected_json['is_user']
+        assert data['is_company'] == expected_json['is_company']
+
+    def test_accounts_create(self, auto_login, create_client) -> None:
+        expected_json = {
+            "username": create_client.username + '1',
+            "password": create_client.password,
+            "is_user": create_client.is_user,
+            "is_company": create_client.is_company
+        }
+        client, user = auto_login()
+        response = client.post(self.url, data=expected_json, format='json')
+        data = json.loads(response.content)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert data['username'] == expected_json['username']
+        assert data['is_user'] == expected_json['is_user']
+        assert data['is_company'] == expected_json['is_company']
+
+    def test_accounts_delete(self, auto_login, create_client) -> None:
+        client, user = auto_login()
+        response = client.delete(f"{self.url}{create_client.id}/", format='json')
+        assert response.status_code == status.HTTP_204_NO_CONTENT
