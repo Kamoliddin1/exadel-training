563931372064578f13122f174d1490bbcc7300bf
Kamoliddin1 <chkb007147@gmail.com>
2022-06-14 13:02:47 +0500

    some touches

--- crm/crm/config/base.py
+++ crm/crm/config/base.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 AUTH_USER_MODEL = "account.Account"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- crm/crm/config/prod.py
+++ crm/crm/config/prod.py
@@ -2,6 +2,9 @@ from .base import *
 
 DEBUG = env('DEBUG')
 
+STATIC_URL = '/static/static/'
+STATIC_ROOT = '/vol/web/static'
+
 
 ALLOWED_HOSTS = env('SERVERNAMES').split(' ')
 SESSION_COOKIE_SECURE = True
